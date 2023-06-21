73a1398539cc57789047d592c0c7a52b14481bd9
hikihun <skh140@naver.com>
2023-06-21 10:52:28 +0900

    build: 프로젝트 생성

---
+++ src/main/java/com/example/tutorialcicd/TutorialCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorialcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialCicdApplication.class, args);
+    }
+
+}
